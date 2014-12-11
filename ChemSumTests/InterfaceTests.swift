97e4cf48583aafdc9cab0f4a55e3e2fb8beb70c6
Anson Jablinski <ansonjablinski@Alfred-2.local>
2014-12-11 09:28:27 -0600

    Initial commit and synx

---
+++ ChemSumTests/InterfaceTests.swift
@@ -0,0 +1,15 @@
+//
+//  InterfaceTests.swift
+//  ChemSum
+//
+//  Created by Anson Jablinski on 12/11/2014.
+//  Copyright (c) 2014 _ansonj. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class InterfaceTests: XCTestCase {
+    
+    
+}
